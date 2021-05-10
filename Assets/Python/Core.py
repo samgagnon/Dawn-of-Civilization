efbf47e2e592477a00248a7b630ec312544a47ac
dguenms <dguenms@gmail.com>
2021-05-10 22:59:12 +0200

    Add victory tooltip and tile overlay

--- Assets/Python/Core.py
+++ Assets/Python/Core.py
@@ -29,6 +29,10 @@ game = gc.getGame()
 map = gc.getMap()
 
 
+def since(iTurn):
+	return turn() - iTurn
+
+
 def bool_metric(func, *args):
 	return lambda value: int(func(value, *args))
 
@@ -1966,6 +1970,16 @@ class Infos:
 	
 	def leaders(self):
 		return InfoCollection.of(gc.getLeaderHeadInfo, gc.getNumLeaderHeadInfos())
+	
+	# TODO: test
+	def paganReligion(self, identifier):
+		if isinstance(identifier, Civ):
+			return self.paganReligion(self.civ(identifier).getPaganReligion())
+	
+		return gc.getPaganReligionInfo(identifier)
+	
+	def paganReligions(self):
+		return InfoCollection.type(gc.getPaganReligionInfo, gc.getNumPaganReligionInfos())
 		
 	def promotion(self, identifier):
 		return gc.getPromotionInfo(identifier)
